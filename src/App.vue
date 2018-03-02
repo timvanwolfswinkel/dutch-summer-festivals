541fb1b79deb346f476220d0a60c6f3132f4ceb5
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-03-02 16:02:24 +0000

    Basic styling settings

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,9 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "scss/global.scss";
+
 .fade-enter-active,
 .fade-leave-active {
   transition-property: opacity;
