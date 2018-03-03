5015c01725e76bb8521fdecee57094f979b52f1f
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-03-03 13:37:56 +0000

    Styling festivals list

--- src/components/FestivalsList.vue
+++ src/components/FestivalsList.vue
@@ -2,19 +2,23 @@
   <div 
     class="festivals-list" 
     v-if="!loading">
-    <router-link
-      v-scroll-reveal="{ delay: 500 }" 
+    <div 
       class="festivals-list__item" 
       v-for="festival in festivals" 
-      :key="festival.name"
-      :to=" {name: 'Festival', params: { festivalId : festival.sys.id } }" >
-      <p class="festivals-list__start-date">{{ festival.fields.startDate | dateDay }}</p>
-      <p class="festivals-list__divider"/>
-      <p class="festivals-list__end-date">{{ festival.fields.endDate | dateDayMonth | uppercase }}</p>
-      <img 
-        class="festivals-list__logo" 
-        src="~@/assets/lowlands.png">
-    </router-link>
+      :key="festival.name">
+      <div v-scroll-reveal="{ delay: 500 }">
+        <router-link 
+          :to=" {name: 'Festival', params: { festivalId : festival.sys.id } }" 
+        >
+          <p class="festivals-list__start-date">{{ festival.fields.startDate | dateDay }}</p>
+          <p class="festivals-list__divider"/>
+          <p class="festivals-list__end-date">{{ festival.fields.endDate | dateDayMonth | uppercase }}</p>
+          <img 
+            class="festivals-list__logo" 
+            src="~@/assets/lowlands.png">
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
