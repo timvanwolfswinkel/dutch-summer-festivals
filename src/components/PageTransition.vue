e4eedcdea45587cc46fedc18847c3a0b39bdc63c
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-04-01 22:29:22 +0100

    Page transition adjustments

--- src/components/PageTransition.vue
+++ src/components/PageTransition.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="page-transition">
-    <transition name="test">
-      <div class="page-transition__container"/>
-    </transition>
-  </div>
+  <div 
+    ref="pageTransition"
+    class="page-transition"
+    style="transform:scaleX(0)"/>
 </template>
 
 <script>
+import anime from "animejs";
+
 export default {
-  name: "PageTransition"
+  name: "PageTransition",
+  mounted() {
+    const animatedTransition = anime.timeline();
+    const el = this.$refs.pageTransition;
+
+    animatedTransition
+      .add({
+        targets: el,
+        duration: 600,
+        delay: 500,
+        scaleX: 1,
+        easing: "easeOutExpo",
+        complete() {
+          el.style.transformOrigin = "0 100%";
+        }
+      })
+      .add({
+        targets: el,
+        duration: 600,
+        delay: 200,
+        scaleX: 0,
+        easing: "easeOutExpo"
+      });
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../scss/components/page-transition.scss";
-
-.test-enter-active,
-.test-leave-active {
-  transition: width 3s;
-}
-
-.test-enter,
-.test-leave-to {
-  width: 0%;
-}
-
-.test-enter-to,
-.test-leave {
-  width: 100%;
-}
 </style>
