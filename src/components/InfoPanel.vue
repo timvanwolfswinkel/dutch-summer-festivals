34496181c72c49e10e5eec72e89f1d45a61e4b37
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-04-06 09:10:28 +0100

    Added close button to info panel

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="info-panel">
-        <div class="info-panel__close"/>
+        <div class="info-panel__close">
+            <img
+                :src="require(`@/assets/img/icons/close.svg`)"
+                class="info-panel__close-icon">
+        </div>
         <div class="info-panel__content">
             <span class="info-panel__heading">Festival</span>
             <span class="info-panel__heading-small">Date</span>
@@ -9,6 +13,12 @@
             <span class="info-panel__heading-medium">€80 - €120</span>
             <span class="info-panel__heading-small">About</span>
             <p class="info-panel__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nihil asperiores fugiat, vitae cupiditate numquam suscipit hic possimus, doloremque repudiandae cum molestias amet velit voluptates ut quas! Perspiciatis, autem ut.</p>
+            <div class="info-panel__learn-more">
+                <span class="info-panel__heading-small info-panel__heading-small--inline">Learn more</span>
+                <img
+                    :src="require(`@/assets/img/icons/learn-more.svg`)"
+                    class="info-panel__icon">
+            </div>
         </div>
     </div>
 </template>
