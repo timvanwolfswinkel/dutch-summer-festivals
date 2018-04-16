a3ed2284035742d341e9328c90711f7aef1cb619
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-04-16 21:03:35 +0100

    Scaling on mousedown and mouseup

--- src/components/MouseBackground.vue
+++ src/components/MouseBackground.vue
@@ -15,28 +15,38 @@ export default {
   },
   mounted() {
     document.body.addEventListener("mousedown", this.onMouseDown);
+    document.body.addEventListener("mouseup", this.onMouseUp);
     document.body.addEventListener("mousemove", this.onMouseMove);
   },
   beforeDestroy() {
     document.body.removeEventListener("mousedown", this.onMouseDown);
+    document.body.removeEventListener("mouseup", this.onMouseUp);
     document.body.removeEventListener("mousemove", this.onMouseMove);
   },
   methods: {
-    onMouseDown() {
-      //   console.log("onMouseDown");
-      //   this.xPos = e.clientX;
-      //   this.yPos = e.clientY;
-      //   console.log(this.xPos);
-      //   console.log(this.yPos);
-      //   console.log(this.$el);
-      //   anime({
-      //     targets: this.$el,
-      //     duration: 250,
-      //     delay: 50,
-      //     left: this.xPos - 50,
-      //     top: this.yPos - 50,
-      //     easing: "easeOutExpo"
-      //   });
+    onMouseDown(e) {
+      this.xPos = e.clientX;
+      this.yPos = e.clientY;
+
+      anime({
+        targets: this.$el,
+        duration: 500,
+        delay: 50,
+        scale: 0.5,
+        easing: "easeOutExpo"
+      });
+    },
+    onMouseUp(e) {
+      this.xPos = e.clientX;
+      this.yPos = e.clientY;
+
+      anime({
+        targets: this.$el,
+        duration: 500,
+        delay: 50,
+        scale: 1,
+        easing: "easeOutExpo"
+      });
     },
     onMouseMove(e) {
       this.xPos = e.clientX;
