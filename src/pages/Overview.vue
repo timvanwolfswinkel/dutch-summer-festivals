1336c9c2cef6716a8fd0c120a2e7dacc3dae0d52
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-04-19 19:32:43 +0100

    Moved dragging events to overview component

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,14 +1,15 @@
 <template>
-  <div 
-    v-drag-scroll
+  <div
     :id="'overview'"
-    class="overview">
+    class="overview"
+    @mousedown="startDrag" 
+    @mousemove="onDrag"
+    @mouseup="stopDrag">
     <intro-page :show-intro="showIntro"/>
     <months-list 
       :show-months-panel="showMonthsPanel" 
       :festivals="festivals"/>
     <festival-info :show-info-panel="showInfoPanel"/>
-    
     <span 
       class="overview__show-months"
       @click="toggleShowMonthsPanel">
@@ -27,7 +28,6 @@ import IntroPage from "../components/IntroPage";
 import List from "../components/overview/List";
 import MonthsList from "../components/panel/MonthsList";
 import FestivalInfo from "../components/panel/FestivalInfo";
-import dragScroll from "../directives/dragScroll";
 
 const mapState = Vuex.mapState;
 
@@ -39,8 +39,13 @@ export default {
     MonthsList,
     FestivalInfo
   },
-  directives: {
-    dragScroll
+  data() {
+    return {
+      startX: 0,
+      diffX: 0,
+      dragging: false,
+      start: 1
+    };
   },
   computed: {
     ...mapState(["loading"]),
@@ -61,6 +66,38 @@ export default {
       }
 
       this.$store.dispatch("toggleMonthsPanel");
+    },
+    startDrag(e) {
+      // TODO: Fix stuttering with lodash throttle: https://medium.com/vuejs-tips/lodash-throttle-b9baf3213f70
+      const event = e || window.event; // check for IE
+
+      this.startX = event.clientX + this.$el.scrollLeft;
+      this.diffX = 0;
+      this.dragging = true;
+    },
+    onDrag(e) {
+      if (this.dragging === true) {
+        const event = e || window.event;
+
+        this.diffX = this.startX - (event.clientX + this.$el.scrollLeft);
+        this.$el.scrollLeft += this.diffX;
+      }
+    },
+    stopDrag() {
+      this.dragging = false;
+      // this.animate();
+    },
+    animate() {
+      const step = Math.sin(this.start);
+
+      if (step <= 0) {
+        window.cancelAnimationFrame(this.animate());
+        this.start = 1;
+      } else {
+        this.$el.scrollLeft += this.diffX * step;
+        this.start -= 0.02;
+        window.requestAnimationFrame(this.animate());
+      }
     }
   }
 };
