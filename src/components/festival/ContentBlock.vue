dd014c0e5fe2faacb00067b6f0de581c20f5fc35
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-05-06 23:07:30 +0100

    Added content block component

---
+++ src/components/festival/ContentBlock.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="content-block">
+    <p class="content-block__title">{{ title }}</p>
+    <p class="content-block__content">{{ content }}</p>
+    <div 
+      v-if="showButton" 
+      class="content-block__button">
+      <button-link 
+        :url="buttonUrl"
+        :text="buttonText" 
+        :emoji="buttonEmoji"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonLink from "../../components/ButtonLink";
+
+export default {
+  name: "ContentBlock",
+  components: {
+    ButtonLink
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+      validate(value) {
+        return [value];
+      }
+    },
+    content: {
+      type: String,
+      required: true,
+      validate(value) {
+        return [value];
+      }
+    },
+    showButton: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validate(value) {
+        return [value];
+      }
+    },
+    buttonText: {
+      type: String,
+      required: false,
+      default: "",
+      validate(value) {
+        return [value];
+      }
+    },
+    buttonEmoji: {
+      type: String,
+      required: false,
+      default: "",
+      validate(value) {
+        return [value];
+      }
+    },
+    buttonUrl: {
+      type: String,
+      required: false,
+      default: "",
+      validate(value) {
+        return [value];
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/scss/components/festival/content-block.scss";
+</style>
--- src/pages/Festival.vue
+++ src/pages/Festival.vue
@@ -5,21 +5,37 @@
       <div 
         v-if="festival"
         class="festival__item">
-        <h1 class="festival__title">{{ festival.name }}</h1>
-        <!-- <span class="festival__date">{{ festival.startDate | dateDay }} - {{ festival.endDate | dateDayMonth | uppercase }}</span> -->
+        <div class="festival__header">
+          <h1 class="festival__title">{{ festival.name }}</h1>
+          <div class="festival__buy-tickets">
+            <button-link 
+              :url="festival.ticketUrl"
+              text="buy tickets" 
+              emoji="💸"/>
+          </div>
+        </div>
         <p class="festival__description">{{ festival.description }}</p>
-        <button-link 
-          :url="festival.ticketUrl"
-          text="buy tickets" 
-          emoji="💸"/>
-        <button-link 
-          :url="festival.ticketUrl"
-          text="view aftermovie" 
-          emoji="👀"/>
-        <button-link 
-          :url="festival.ticketUrl"
-          text="open on google maps" 
-          emoji="🗺"/>
+
+        <content-block 
+          :content="festival.locationName"
+          :show-button="true"
+          :button-url="festival.ticketUrl"
+          title="where"
+          button-text="open on google maps"
+          button-emoji="🗺"
+        />
+
+        <content-block 
+          content="8juni"
+          title="when"
+        />
+
+        <div class="festival__when">
+          <p class="festival__when-title">When</p>
+          <p class="festival__where-location">
+            {{ festival.startDate | dateDay }} - {{ festival.endDate | dateDayMonthFull | uppercase }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -30,13 +46,20 @@ import store from "../store/store";
 
 import ButtonBackToOverview from "../components/ButtonBackToOverview";
 import ButtonLink from "../components/ButtonLink";
+import ContentBlock from "../components/festival/ContentBlock";
 
 export default {
   store,
   name: "Festival",
   components: {
     ButtonBackToOverview,
-    ButtonLink
+    ButtonLink,
+    ContentBlock
+  },
+  data() {
+    return {
+      date: ""
+    };
   },
   computed: {
     festival() {
@@ -59,6 +82,12 @@ export default {
       const festival = festivals.filter(key => key.fields.url === festivalId);
 
       console.log(festival);
+
+      // moment.locale("en");
+      // const endDate = moment(festival.EndDate, "YYYY-MM-DD").format("DD MMMM");
+      // const startDate = moment(festival.startDate, "YYYY-MM-DD").format("DD");
+
+      // this.date = `${startDate} - ${endDate}`;
 
       return festival.length !== 0 ? festival[0].fields : {};
     }
