4711c3703a36c5c5dd846f9bd6e20dd71d1cb1c3
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-05-07 20:54:28 +0100

    Added base setup for artists ticker

---
+++ src/components/festival/ArtistsTicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="artists-ticker">
+    ArtistsTicker
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArtistsTicker",
+  props: {
+    artists: {
+      type: Object,
+      required: false,
+      default: () => {},
+      validate(value) {
+        return [value];
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/scss/components/festival/artists-ticker.scss";
+</style>
