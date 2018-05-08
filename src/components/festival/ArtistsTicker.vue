e40b6036c6a60d77bee97c6836c58bc013e96f52
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-05-08 08:54:52 +0100

    Base setup for artists ticker

--- src/components/festival/ArtistsTicker.vue
+++ src/components/festival/ArtistsTicker.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="artists-ticker">
-    ArtistsTicker
+    <div
+        v-for="(row, index) in artistsInChunks"
+        :key="`artists-row-${index}`"  
+        :class="`artists-ticker__row artists-ticker__row--${index}`">
+        <div
+        v-for="(artist, index) in row"
+        :key="`artist-${index}`"  
+        :class="`artists-ticker__artist`">
+          <span>
+            {{ artist.name }}
+          </span>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -14,6 +26,50 @@ export default {
       default: () => {},
       validate(value) {
         return [value];
+      }
+    }
+  },
+  data() {
+    return {
+      // TODO: fill this data with props artists
+      artistsTemp: [
+        { name: "Kendrick Lamar", imageUrl: "1" },
+        { name: "Jeugd van Tegenwoordig", imageUrl: "2" },
+        { name: "San Proper", imageUrl: "3" },
+        { name: "Honey Dijon", imageUrl: "4" },
+        { name: "Fatima Yamaha", imageUrl: "5" },
+        { name: "Job Jobse", imageUrl: "6" },
+        { name: "Grizzly Bear", imageUrl: "7" },
+        { name: "Dua Lipa", imageUrl: "8" },
+        { name: "N.E.R.D.", imageUrl: "9" },
+        { name: "The War On Drugs", imageUrl: "10" },
+        { name: "Bicep", imageUrl: "11" },
+        { name: "Actress", imageUrl: "12" },
+        { name: "Mr. Fingers", imageUrl: "13" },
+        { name: "Max Cooper", imageUrl: "14" },
+        { name: "Rival Consoles", imageUrl: "15" },
+        { name: "DJ Koze", imageUrl: "16" },
+        { name: "Django Django", imageUrl: "17" },
+        { name: "Alt-J", imageUrl: "18" },
+        { name: "Febueder", imageUrl: "19" },
+        { name: "Moscoman", imageUrl: "20" }
+      ],
+      artistsInChunks: []
+    };
+  },
+  beforeMount() {
+    this.divideArtistsInChunks(this.artistsTemp);
+  },
+  methods: {
+    divideArtistsInChunks(artists) {
+      const splitSize = Math.floor(artists.length / 3);
+      const maxSize = 2;
+      let i;
+
+      for (i = 0; i < artists.length; i += splitSize) {
+        if (!(this.artistsInChunks.length > maxSize)) {
+          this.artistsInChunks.push(artists.slice(i, i + splitSize));
+        }
       }
     }
   }
