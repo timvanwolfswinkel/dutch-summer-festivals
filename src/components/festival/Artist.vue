41dfa94b3bea53e8afe760d937b17c921abc09cc
Tim van Wolfswinkel <tvanwolfswinkel@gmail.com>
2018-05-10 13:29:27 +0100

    Added some more props (songTitle & songUrl) to the artist component

--- src/components/festival/Artist.vue
+++ src/components/festival/Artist.vue
@@ -2,7 +2,7 @@
   <div class="artist">
     <div class="artist__container">
       <div class="artist__info">
-        <span class="artist__song">R u mine?</span>
+        <span class="artist__song">{{ songTitle }}</span>
         <span class="artist__name">{{ artist }}</span>
         <span class="artist__title">Will be there</span>
       </div>
@@ -23,6 +23,22 @@ export default {
       type: String,
       required: false,
       default: "arctic monkeys",
+      validate(value) {
+        return [value];
+      }
+    },
+    songTitle: {
+      type: String,
+      required: false,
+      default: "R u mine?",
+      validate(value) {
+        return [value];
+      }
+    },
+    songUrl: {
+      type: String,
+      required: false,
+      default: "https://p.scdn.co/mp3-preview/104ad0ea32356b9f3b2e95a8610f504c90b0026b?cid=8897482848704f2a8f8d7c79726a70d4",
       validate(value) {
         return [value];
       }
